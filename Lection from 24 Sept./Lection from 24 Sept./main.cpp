25438e49a6306cfd5e7e099e2f104a1fc0169a6b
Roman Esin <esinromanswift@gmail.com>
2020-09-24 08:23:49 +1000

    Initial commit

---
+++ Lection from 24 Sept./Lection from 24 Sept./main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lection from 24 Sept.
+//
+//  Created by Roman Esin on 24.09.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
